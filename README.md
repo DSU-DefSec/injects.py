e714f836bece7c649b4ea87aefacce4a9d4de5d6
BuildTools <unconfigured@null.spigotmc.org>
2020-09-05 17:44:38 -0500

    Added documentation and more command line options
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

---
+++ README.md
@@ -0,0 +1,58 @@
+# injects.py
+Simple script to auto post injects to discord and scoring engine
+
+## Configs:
+
+#### injects.json:
+```json
+{
+  "injects": [
+    {
+      "name": "Name of inject",
+      "desc": "Description of inject",
+      "post": "yyyy-mm-dd hh:mm:ss",
+      "due": "2020-09-05 16:00:00"
+    },
+    { ... }
+  ]
+}
+```
+#### config.json:
+```json
+{
+  "discord": "https://discordapp.com/api/webhooks/##################/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "discord_content": "Whatever you want in the discord message content (not the embed)",
+  "engine": "http://scoring.engine/to/be/implemented/later/webhook.html",
+  "close_enough": 10 // Seconds before post time that inject can be sent (if 0 then could be sent a bit late)
+}
+```
+
+
+## Usage:
+#### Standalone:
+Will wait until the right time then post injects  
+Updates `state.json` as it goes. Will start back up in the same spot if restarted
+```
+$ python3 injects.py
+```
+
+#### Single run:
+Will post any injects that should be posted after the last posted `state.json` and before the current time then exit  
+Could crontab this if you dont want the script always running
+```
+$ python3 injects.py <literally anything but a number and all>
+$ python3 injects.py tacocat
+```
+
+#### Specific inject:
+Will post specific inject and exit ignoring `state.json`
+```
+$ python3 injects.py <inject number>
+$ python3 injects.py 3
+```
+
+#### All injects:
+Will post all injects ignoring `state.json` (requires confirmation)
+```
+$ python3 injects.py all
+```
--- injects.py
+++ injects.py
@@ -1,42 +1,61 @@
-import json
-import sys
 from datetime import datetime, timedelta, timezone
+from json import load, dump, dumps
 from os.path import dirname, exists
+from sys import argv as sys_args
 from time import sleep
 
-import requests
+from requests import post
 
-standalone = len(sys.argv) == 1
+standalone = len(sys_args) == 1
 print(f"Starting in {('stateless', 'standalone')[standalone]} mode")
+specific = -1
+send_all = False
+if not standalone:
+    if sys_args[1] == "all":
+        if input("Send all injects? (yes):") != "yes":
+            exit()
+        send_all = True
+    else:
+        try:
+            specific = int(sys_args[1])
+        except ValueError:
+            specific = -1
 
 dir = dirname(__file__)
 
-injects = json.load(open(dir + "/injects.json", 'r'))
-config = json.load(open(dir + "/config.json", 'r'))
+injects = load(open(dir + "/injects.json", 'r'))
+config = load(open(dir + "/config.json", 'r'))
 if exists(dir + "/status.json"):
-    status = json.load(open(dir + "/status.json", 'r'))
+    status = load(open(dir + "/state.json", 'r'))
 else:
-    status = {"last_sent": 0}
+    status = {"last_sent": -1}
 
 close_enough = timedelta(seconds=config["close_enough"])
 local_timezone = timezone(datetime.now() - datetime.utcnow())
 
 for inject_number, inject in enumerate(injects["injects"]):
-
-    # Skip if already sent
-    if inject_number <= status["last_sent"]:
-        continue
-
-    target = datetime.strptime(inject["post"], "%Y-%m-%d %H:%M:%S")
-    if standalone:
-        while target > datetime.now() + close_enough:
-            sleep_sec = int((target - datetime.now()).total_seconds() + 1)
-            print(f"Sleeping {sleep_sec} seconds before next check")
-            sleep(sleep_sec)
-    else:
-        # Skip if it is for more than 1 minute from now
-        if target > datetime.now() + close_enough:
+    if not send_all:
+        # Skip if already sent
+        if inject_number <= status["last_sent"]:
             continue
+
+        target = datetime.strptime(inject["post"], "%Y-%m-%d %H:%M:%S")
+        if standalone:
+            # Wait until time to send it
+            # We assume injects are in chronological order
+            while target > datetime.now() + close_enough:
+                sleep_sec = int((target - datetime.now()).total_seconds() + 1)
+                print(f"Sleeping {sleep_sec} seconds before next check")
+                sleep(sleep_sec)
+        else:
+            if specific == -1:
+                # Skip if it is not time yet
+                if target > datetime.now() + close_enough:
+                    continue
+            else:
+                # If we want a specific one only send that one
+                if inject_number != specific:
+                    continue
 
     # Should send now
     discord = {
@@ -63,9 +82,10 @@ for inject_number, inject in enumerate(injects["injects"]):
 
     print(f"Posting inject {inject_number}: {inject}")
 
-    print(requests.post(config["discord"], data=json.dumps(discord), headers={"Content-Type": "application/json"}))
-    status["last_sent"] = inject_number
-    # requests.post(webhooks["engine"], data=json.dumps({
+    print(post(config["discord"], data=dumps(discord), headers={"Content-Type": "application/json"}))
+    # post(webhooks["engine"], data=json.dumps({
     #     "idk_what": "to_put_here", "inject": num
     # }), headers={"Content-Type": "application/json"})
-    json.dump(status, open("status.json", 'w'))
+    if not send_all and specific == -1:
+        status["last_sent"] = inject_number
+        dump(status, open("state.json", 'w'))
